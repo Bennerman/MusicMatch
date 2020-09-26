997acf477e7b41089d1ab07153879aa428ab0396
Ben Radesovich <bpradosevich@gmail.com>
2020-09-25 23:26:15 -0500

    Update FindHoroscope.py

--- FindHoroscope.py
+++ FindHoroscope.py
@@ -1,7 +1,16 @@
-#TODO Horoscope API info
+import json
+import requests
 
-key = ef5ff5341ab06ee976da5fa46ee2c6420c3c6e55105b79e37e032cd2953e8a0e
-base_url = "https://api.prokerala.com/dashboard/home"
+r = requests.get(url="http://horoscope-api.herokuapp.com/horoscope/today/Libra")
+
+data = r.json()
+
+print(data)
+
+#key = "ef5ff5341ab06ee976da5fa46ee2c6420c3c6e55105b79e37e032cd2953e8a0e"
+#base_url = "https://api.prokerala.com/dashboard/home"
+
+
 
 print("Hello casey and alex")
 
