e17a04acf04aabfbd7401645bf6e09ba79c7df54
alexdwu3 <alexdwu3@gmail.com>
2020-09-25 22:00:03 -0500

    Update FindHoroscope.py

--- FindHoroscope.py
+++ FindHoroscope.py
@@ -1,5 +1,8 @@
 #TODO Horoscope API info
 
+key = ef5ff5341ab06ee976da5fa46ee2c6420c3c6e55105b79e37e032cd2953e8a0e
+base_url = "https://api.prokerala.com/dashboard/home"
+
 print("Hello casey and alex")
 
 print("we should smoke in the next 2 hours")
